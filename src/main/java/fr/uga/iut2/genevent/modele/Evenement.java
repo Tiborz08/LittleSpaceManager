b1149a154988ceeb3c573d914c576e23d2e23eda
torrejoc1 <cyrian.torrejon@etu.univ-grenoble-alpes.fr>
2024-06-14 10:38:31 +0200

    Ajout Attributs Event + construccteur

--- src/main/java/fr/uga/iut2/genevent/modele/Evenement.java
+++ src/main/java/fr/uga/iut2/genevent/modele/Evenement.java
@@ -1,4 +1,31 @@
 package fr.uga.iut2.genevent.modele;
 
-public class Evenement {
+import java.util.Date;
+
+public abstract class Evenement {
+
+    //ATTRIBUTIONS
+
+    private int idEvent;
+    private String nom;
+    private int capaciteParticipants;
+    private int capaciteSpectateur;
+    private float coutInitial;
+    private float prixTickets;
+    private Date debut;
+    private Date fin;
+
+    //CONSTRUCTEUR(S)
+
+    public Evenement(String nom, int capaciteParticipants, int capaciteSpectateur, float coutInitial, float prixTickets, Date debut, Date fin) {
+        this.idEvent=newId();
+        this.nom = nom;
+        this.capaciteParticipants = capaciteParticipants;
+        this.capaciteSpectateur = capaciteSpectateur;
+        this.coutInitial = coutInitial;
+        this.prixTickets = prixTickets;
+        this.debut = debut;
+        this.fin = fin;
+    }
+
 }
