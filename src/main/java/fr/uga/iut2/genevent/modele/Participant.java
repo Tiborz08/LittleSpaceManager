2127c1c0425ba577a01c5c2967eaacbc73513f40
allegreb <bastien.allegre@etu.univ-grenoble-alpes.fr>
2024-06-14 11:48:29 +0200

    Ajout liens avec Evenement dans la classe Participant.

--- src/main/java/fr/uga/iut2/genevent/modele/Participant.java
+++ src/main/java/fr/uga/iut2/genevent/modele/Participant.java
@@ -1,14 +1,19 @@
 package fr.uga.iut2.genevent.modele;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public abstract class Participant extends Personne {
 
+    private ArrayList<Evenement> evenements=new ArrayList<>();
     private float salaire;
 
+    //CONSTRUCTEUR
     public Participant(String nom,String prenom,float salaire){
         super(nom,prenom);
         this.setSalaire(salaire);
     }
-
+    //SETTER
     /**
      * Définit la valeur du salaire d'un participant. Si le salaire du participant est négatif, alors le salaire est égal à 0.
      * @param salaire Réel à virgule qui représente le salaire du participant.
@@ -18,5 +23,20 @@ public abstract class Participant extends Personne {
             this.salaire=0;
         }else {
         this.salaire = salaire;}
+    }
+
+    //GETTER
+    public float getSalaire() {
+        return salaire;
+    }
+    public ArrayList<Evenement> getEvenements() {
+        return evenements;
+    }
+    //METHODE
+    public void addEvenement(Evenement evenement){
+        if (!evenements.contains(evenement)) {
+            this.evenements.add(evenement);
+            evenement.addParticipant(this);
+        }
     }
 }
