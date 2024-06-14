a1d68d0bcbd7c662e191734599bcd9e27c5efc97
Yvan D'ETTORRE <yvan.d-ettorre@etu.univ-grenoble-alpes.fr>
2024-06-14 14:49:54 +0200

    La méthode capitalize mets tous les charactères en minuscule à l'exception du 1er

--- src/main/java/fr/uga/iut2/genevent/util/LittleSpaceManager_Utilitaire.java
+++ src/main/java/fr/uga/iut2/genevent/util/LittleSpaceManager_Utilitaire.java
@@ -14,7 +14,7 @@ public class LittleSpaceManager_Utilitaire {
         if (str == null || str.isEmpty()) {
             return str;
         }
-        return str.substring(0, 1).toUpperCase() + str.substring(1);
+        return str.substring(0, 1).toUpperCase() + str.substring(1).toLowerCase();
     }
 
 }
