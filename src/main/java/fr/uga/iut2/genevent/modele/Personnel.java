87af1c35eb814989f6847a4c7c5d88b8d0583471
bastien <bastien.allegre@etu.univ-grenoble-alpes.fr>
2024-06-14 14:26:06 +0200

    Ajout de la classe Personnel
    - Ajout de la classe `Personnel` héritant de `Participant`
    - Ajout d'un constructeur prenant en paramètres `nom`, `prenom` et `salaire`

---
+++ src/main/java/fr/uga/iut2/genevent/modele/Personnel.java
@@ -0,0 +1,9 @@
+package fr.uga.iut2.genevent.modele;
+
+public class Personnel extends Participant {
+
+    public Personnel(String nom,String prenom,float salaire){
+        super(nom,prenom,salaire);
+    }
+
+}
