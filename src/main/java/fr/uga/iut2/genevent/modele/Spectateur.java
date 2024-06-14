ad4c1bac7409853590723c9eecfa5be13b924aef
Matis MALANDRINO <matis.malandrino@etu.univ-grenoble-alpes.fr>
2024-06-14 15:00:57 +0200

    Ajout de la liste de ticket et de de la méthode addTicket dans la classe Spectateur.

--- src/main/java/fr/uga/iut2/genevent/modele/Spectateur.java
+++ src/main/java/fr/uga/iut2/genevent/modele/Spectateur.java
@@ -3,16 +3,36 @@ package fr.uga.iut2.genevent.modele;
 import java.util.ArrayList;
 
 public class Spectateur extends Personne{
-    private ArrayList<Evenement> evenements=new ArrayList<>();
+    private ArrayList<Ticket> tickets = new ArrayList<>();
     //CONSTRUCTEUR
     public Spectateur(String nom,String prenom){
         super(nom,prenom);
     }
 
     //GETTER
-    public ArrayList<Evenement> getEvenements() {
-        return evenements;
+
+    /**
+     * Cette fonction nous permet de récupérer la liste des tickets du spectateur.
+     * @return
+     */
+
+    public ArrayList<Ticket> getTickets() {
+        return tickets;
     }
+
+    /**
+     * La fonction permet d'ajouter un ticket dans la liste de ticket d'un spectateur. Au préalable la fonction vérifie que le ticket n'est pas déjà dans la liste.
+     * @param ticket
+     */
+
+    public void addTicket(Ticket ticket){
+        if (!tickets.contains(ticket)){
+            tickets.add(ticket);
+        }
+    }
+
     //METHODE
+
+
 
 }
