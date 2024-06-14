dfa6d502214c0a84726e605ffba4324153b9430c
allegreb <bastien.allegre@etu.univ-grenoble-alpes.fr>
2024-06-14 11:48:09 +0200

    Ajout liens avec Evenement dans la classe spectateur.

--- src/main/java/fr/uga/iut2/genevent/modele/Spectateur.java
+++ src/main/java/fr/uga/iut2/genevent/modele/Spectateur.java
@@ -1,8 +1,18 @@
 package fr.uga.iut2.genevent.modele;
 
-public class Spectateur extends Personne{
+import java.util.ArrayList;
 
+public class Spectateur extends Personne{
+    private ArrayList<Evenement> evenements=new ArrayList<>();
+    //CONSTRUCTEUR
     public Spectateur(String nom,String prenom){
         super(nom,prenom);
     }
+
+    //GETTER
+    public ArrayList<Evenement> getEvenements() {
+        return evenements;
+    }
+    //METHODE
+
 }
