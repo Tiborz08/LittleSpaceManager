97e922aa01daa5ff3400efe01b17e213bc02db13
Yvan D'ETTORRE <yvan.d-ettorre@etu.univ-grenoble-alpes.fr>
2024-06-14 10:37:24 +0200

    Ajout d'une fonction qui crée un ID

--- src/main/java/fr/uga/iut2/genevent/util/LittleSpaceManager_Utilitaire.java
+++ src/main/java/fr/uga/iut2/genevent/util/LittleSpaceManager_Utilitaire.java
@@ -1,4 +1,13 @@
 package fr.uga.iut2.genevent.util;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class LittleSpaceManager_Utilitaire {
+
+    private AtomicInteger id = new AtomicInteger(0);
+
+    public int newId() {
+        return id.incrementAndGet();
+    }
+
 }
