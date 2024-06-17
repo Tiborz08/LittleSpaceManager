10d58e2e0a3362b9b8ea410b205d93ad3882cc89
Yvan D'ETTORRE <yvan.d-ettorre@etu.univ-grenoble-alpes.fr>
2024-06-17 11:05:30 +0200

    Nouveau fichier main depuis 0

--- src/main/java/fr/uga/iut2/genevent/Main.java
+++ src/main/java/fr/uga/iut2/genevent/Main.java
@@ -1,40 +1,4 @@
 package fr.uga.iut2.genevent;
 
-import fr.uga.iut2.genevent.controleur.OldControleur;
-import fr.uga.iut2.genevent.modele.GenEvent;
-import fr.uga.iut2.genevent.util.Persisteur;
-import java.io.IOException;
-
-
 public class Main {
-
-    public static final int EXIT_ERR_LOAD = 2;
-    public static final int EXIT_ERR_SAVE = 3;
-
-    public static void main(String[] args) {
-        GenEvent genevent = null;
-
-        try {
-            genevent = Persisteur.lireEtat();
-        }
-        catch (ClassNotFoundException | IOException ignored) {
-            System.err.println("Erreur irrécupérable pendant le chargement de l'état : fin d'exécution !");
-            System.err.flush();
-            System.exit(Main.EXIT_ERR_LOAD);
-        }
-
-        OldControleur oldControleur = new OldControleur(genevent);
-        // `Controleur.demarrer` garde le contrôle de l'exécution tant que
-        // l'utilisa·teur/trice n'a pas saisi la commande QUITTER.
-        oldControleur.demarrer();
-
-        try {
-            Persisteur.sauverEtat(genevent);
-        }
-        catch (IOException ignored) {
-            System.err.println("Erreur irrécupérable pendant la sauvegarde de l'état : fin d'exécution !");
-            System.err.flush();
-            System.exit(Main.EXIT_ERR_SAVE);
-        }
-    }
 }
