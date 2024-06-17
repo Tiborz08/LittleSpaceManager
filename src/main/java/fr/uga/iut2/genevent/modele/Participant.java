529115b35b508db2ea0a487cd8372777898a7b82
Yvan D'ETTORRE <yvan.d-ettorre@etu.univ-grenoble-alpes.fr>
2024-06-17 11:03:53 +0200

    removeEvenement supprime l'évenement de la liste des évenements du participant + javadoc

--- src/main/java/fr/uga/iut2/genevent/modele/Participant.java
+++ src/main/java/fr/uga/iut2/genevent/modele/Participant.java
@@ -45,9 +45,15 @@ public abstract class Participant extends Personne {
         }
     }
 
+    /**
+     * Retire un événement de la liste des événements auxquels participe le participant, en vérifiant s'il est présent
+     * Informe l'événement que le participant n'est plus un de ses participants.
+     * @param evenement Un événement auquel le participant participe.
+     */
     public void removeEvenement(Evenement evenement) {
         if (evenements.contains(evenement)) {
             evenement.removeParticipant(this);
+            evenements.remove(evenement);
         }
     }
 }
