ee8b9028cd7744a7b7ce37f6d69beebeb4da7c7b
Djibril MILIANI <djibril.miliani@etu.univ-grenoble-alpes.fr>
2024-06-17 10:13:46 +0200

    ajout liste MainControleur.java

--- src/main/java/fr/uga/iut2/genevent/controleur/MainControleur.java
+++ src/main/java/fr/uga/iut2/genevent/controleur/MainControleur.java
@@ -1,4 +1,16 @@
 package fr.uga.iut2.genevent.controleur;
 
+import fr.uga.iut2.genevent.modele.*;
+
+import java.util.ArrayList;
+
 public class MainControleur {
+
+    private ArrayList<Evenement> evenements = new ArrayList<>();
+    private ArrayList<Artiste> artistes = new ArrayList<>();
+    private ArrayList<Spectateur> spectateurs = new ArrayList<>();
+    private ArrayList<Personnel> personnels = new ArrayList<>();
+    private ArrayList<Salle> salles = new ArrayList<>();
+
+
 }
