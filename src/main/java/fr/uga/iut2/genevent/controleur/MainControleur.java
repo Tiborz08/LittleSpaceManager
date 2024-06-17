d44160e821a3a6ecf6a9b7c3e4d67ff20ff049d1
Djibril MILIANI <djibril.miliani@etu.univ-grenoble-alpes.fr>
2024-06-17 10:42:48 +0200

    ajout des id de toutes les pages création + début de MainControleur et CreationControleur + il faudra ajouter les controleurs

--- src/main/java/fr/uga/iut2/genevent/controleur/MainControleur.java
+++ src/main/java/fr/uga/iut2/genevent/controleur/MainControleur.java
@@ -1,6 +1,10 @@
 package fr.uga.iut2.genevent.controleur;
 
 import fr.uga.iut2.genevent.modele.*;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 import java.util.ArrayList;
 
@@ -12,5 +16,36 @@ public class MainControleur {
     private ArrayList<Personnel> personnels = new ArrayList<>();
     private ArrayList<Salle> salles = new ArrayList<>();
 
+    public void ajouterEvenement(Evenement evenement) {
+        evenements.add(evenement);
+    }
 
+    public void ajouterArtiste(Artiste artiste) {
+        artistes.add(artiste);
+    }
+
+    public void ajouterSpectateur(Spectateur spectateur) {
+        spectateurs.add(spectateur);
+    }
+
+    public void ajouterPersonnel(Personnel personnel) {
+        personnels.add(personnel);
+    }
+
+    public void ajouterSalle(Salle salle) {
+        salles.add(salle);
+    }
+
+    public void ouvrirFenetreCreation(String typeCreation) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fr/uga/iut2/genevent/vue/CreationControleur.fxml"));
+        Parent root = loader.load();
+
+        CreationControleur creationController = loader.getController();
+        creationController.setMainController(this);
+
+        Stage stage = new Stage();
+        stage.setTitle("Création de " + typeCreation);
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
 }
