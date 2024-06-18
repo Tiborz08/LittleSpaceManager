5ae1f0f44c9d5f59df44f21178482cd7d36e5448
torrejoc1 <cyrian.torrejon@etu.univ-grenoble-alpes.fr>
2024-06-18 14:46:21 +0200

    Modification du main java
    - correction d'un test

--- src/main/java/fr/uga/iut2/genevent/Main.java
+++ src/main/java/fr/uga/iut2/genevent/Main.java
@@ -21,7 +21,7 @@ public class Main extends Application {
 
         @Override
         public void start(Stage stage) throws IOException {
-            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("vue/OptionEvenementView.fxml"));
+            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("vue/AccueilView.fxml"));
             Scene scene = new Scene(fxmlLoader.load());
             stage.setTitle("LittleSpaceManager");
             stage.getIcons().add( new Image(String.valueOf(getClass().getResource("/fr/uga/iut2/genevent/vue/logo/logo-lsm.png"))));
