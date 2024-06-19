b3d28c523d4c4bce799dd1664cdd0f653e13b781
torrejoc1 <cyrian.torrejon@etu.univ-grenoble-alpes.fr>
2024-06-19 16:26:40 +0200

    Ajouter la fonctionnalité des specs pour les concerts
    - Le bouton et maintenant fonctionnel pour les concerts, et permet de modifier le genre/style de musique de l'événement

--- src/main/java/fr/uga/iut2/genevent/controleur/ModificationEvenementController.java
+++ src/main/java/fr/uga/iut2/genevent/controleur/ModificationEvenementController.java
@@ -21,6 +21,7 @@ import java.io.IOException;
 import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Objects;
 import java.util.function.Consumer;
 
 public class ModificationEvenementController {
@@ -561,7 +562,7 @@ public class ModificationEvenementController {
         }
     }
 
-    //Bouton Spec accessoires
+    //Bouton Spec accessoires + genre
 
     @FXML
     private Button btnValiderAjoutAccessoire;
@@ -575,6 +576,12 @@ public class ModificationEvenementController {
     private TextField tfNomAccessoire;
     @FXML
     private Button btnRetourAjoutAccessoire;
+    @FXML
+    private TextField tfGenre;
+    @FXML
+    private Button btnRetourGenre;
+    @FXML
+    private Button btnValiderGenre;
 
     @FXML
     public void onSpecClick(ActionEvent event){
@@ -594,14 +601,15 @@ public class ModificationEvenementController {
             Scene scene = new Scene(root);
 
             if(evenement instanceof Concert){
-
+                popupStage.setTitle("Modif genre de : "+evenement.getNom());
+                tfGenre.setText(((Concert) evenement).getGenre());
             }else {
                 ObservableList<Accessoire> listeAccessoire = FXCollections.observableArrayList(new ArrayList<>(evenement.getAccessoires()));
                 lvAccessoire.setItems(listeAccessoire);
+                popupStage.setTitle("Ajout à : "+evenement.getNom());
             }
 
             popupStage.setScene(scene);
-            popupStage.setTitle("Ajout à : "+evenement);
             popupStage.show();
 
         }catch (Exception e) {
@@ -649,6 +657,23 @@ public class ModificationEvenementController {
         Stage stage = (Stage) btnRetourAjoutAccessoire.getScene().getWindow();
         stage.close();
     }
+
+    @FXML
+    public void onRetourGenreClick(ActionEvent event){
+        Stage stage = (Stage) btnRetourGenre.getScene().getWindow();
+        stage.close();
+    }
+
+    @FXML
+    public void onValiderGenreClick(ActionEvent event){
+        if(!Objects.equals(tfGenre.getText(), "")){
+            ((Concert) evenement).setGenre(tfGenre.getText());
+        }
+        Stage stage = (Stage) btnValiderGenre.getScene().getWindow();
+        stage.close();
+    }
+
+    //=====================================
 
     @FXML
     private void onSuprPersonne(){
